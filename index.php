369a4036604d6a440e205e1195d3a3e9ae2a3a34
g123udini <g123udini@gmail.com>
2022-08-15 14:48:47 +0300

    Return objects

--- index.php
+++ index.php
@@ -6,12 +6,21 @@ use TaskForce\Task;
 $task = new Task(1, 2);
 
 $array = $task->getAvailableActions(1);
-var_dump($array);
 
-echo assert($task->getNextStatus('action_cancel') == Task::STATUS_CANCELED, 'cancel action');
+echo assert($task->getAvailableActions(1) == [ActionCancel::class], 'object return check');
+
+echo assert($task->getAvailableActions(2) == [ActionAccept::class, ActionRefuse::class], 'object return check');
+
+echo assert($task->getNextStatus(ActionCancel::class) == Task::STATUS_CANCELED, 'cancel action');
+
+echo assert($task->getNextStatus(ActionExecute::class) == Task::STATUS_EXECUTED, 'execute action');
 
 $task->actionAccept(2);
 echo assert($task->getCurrentStatus() == Task::STATUS_IN_WORK, 'accept task');
 
+echo assert($task->getAvailableActions(1) == [ActionExecute::class, ActionCancel::class], 'object return check');
+
 $task->actionCancel(1);
-echo assert($task->getCurrentStatus() == Task::STATUS_CANCELED, 'execute task');
+echo assert($task->getCurrentStatus() == Task::STATUS_CANCELED, 'cancel task');
+
+
