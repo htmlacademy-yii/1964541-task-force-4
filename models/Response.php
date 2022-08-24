1fe0204ca08bfee4cd4faf4f4dcfe0d53660944d
g123udini <g123udini@gmail.com>
2022-08-24 10:41:19 +0300

    generated models

---
+++ models/Response.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "response".
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property int $executor_id
+ * @property string $content
+ * @property int|null $price
+ *
+ * @property User $executor
+ * @property Task $task
+ */
+class Response extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'response';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['task_id', 'executor_id', 'content'], 'required'],
+            [['task_id', 'executor_id', 'price'], 'integer'],
+            [['content'], 'string'],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['task_id' => 'id']],
+            [['executor_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['executor_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'task_id' => 'Task ID',
+            'executor_id' => 'Executor ID',
+            'content' => 'Content',
+            'price' => 'Price',
+        ];
+    }
+
+    /**
+     * Gets query for [[Executor]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getExecutor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'executor_id']);
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['id' => 'task_id']);
+    }
+}
---
+++ models/Task.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "task".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property string|null $file
+ * @property float|null $long
+ * @property float|null $lat
+ * @property int|null $city_id
+ * @property int $price
+ * @property int $customer_id
+ * @property int|null $executor_id
+ * @property string|null $status
+ * @property int $category_id
+ * @property string|null $deadline
+ * @property string|null $dt_add
+ *
+ * @property Category $category
+ * @property City $city
+ * @property User $customer
+ * @property User $executor
+ * @property Response[] $responses
+ * @property Review[] $reviews
+ */
+class Task extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'task';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'price', 'customer_id', 'category_id'], 'required'],
+            [['description', 'status'], 'string'],
+            [['long', 'lat'], 'number'],
+            [['city_id', 'price', 'customer_id', 'executor_id', 'category_id'], 'integer'],
+            [['deadline', 'dt_add'], 'safe'],
+            [['title'], 'string', 'max' => 128],
+            [['file'], 'string', 'max' => 320],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'id']],
+            [['executor_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['executor_id' => 'id']],
+            [['customer_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['customer_id' => 'id']],
+            [['city_id'], 'exist', 'skipOnError' => true, 'targetClass' => City::className(), 'targetAttribute' => ['city_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'file' => 'File',
+            'long' => 'Long',
+            'lat' => 'Lat',
+            'city_id' => 'City ID',
+            'price' => 'Price',
+            'customer_id' => 'Customer ID',
+            'executor_id' => 'Executor ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+            'deadline' => 'Deadline',
+            'dt_add' => 'Dt Add',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[City]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCity()
+    {
+        return $this->hasOne(City::className(), ['id' => 'city_id']);
+    }
+
+    /**
+     * Gets query for [[Customer]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCustomer()
+    {
+        return $this->hasOne(User::className(), ['id' => 'customer_id']);
+    }
+
+    /**
+     * Gets query for [[Executor]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getExecutor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'executor_id']);
+    }
+
+    /**
+     * Gets query for [[Responses]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getResponses()
+    {
+        return $this->hasMany(Response::className(), ['task_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Reviews]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReviews()
+    {
+        return $this->hasMany(Review::className(), ['task_id' => 'id']);
+    }
+}
