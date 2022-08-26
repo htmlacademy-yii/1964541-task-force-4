57a22db3c1ad29368b2dc08e19a294dea3e3a5b3
g123udini <g123udini@gmail.com>
2022-08-26 12:46:03 +0300

    ?

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -3,11 +3,15 @@
 namespace app\controllers;
 
 use app\models\Task;
+use Yii;
 use yii\web\Controller;
 
 class TaskController extends Controller
 {
     public function actionIndex() {
+        if (Yii::$app->request->getIsPost()) {
+            $filterForm = Yii::$app->request->post();
+        }
         $activeQuery = Task::find();
         $activeQuery->joinWith('city');
         $activeQuery->joinWith('category');
