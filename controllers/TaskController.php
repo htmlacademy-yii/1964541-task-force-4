7421e7ea7d1e4220339bc71f4b7f4cdf4d2c595d
g123udini <g123udini@gmail.com>
2022-08-29 10:22:24 +0300

    d

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\Task;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 class TaskController extends Controller
 {
@@ -15,6 +16,18 @@ class TaskController extends Controller
         $activeQuery->orderBy(['dt_add' => SORT_ASC]);
         $tasks = $activeQuery->all();
         return $this->render('task', ['tasks' => $tasks]);
+    }
+
+    public function actionView($id) {
+        $activeQuery = Task::find();
+        $activeQuery->joinWith('city');
+        $activeQuery->joinWith('category');
+        $activeQuery->where(['task.id' => $id]);
+        $task = $activeQuery->one();
+        if (!$task) {
+            throw new NotFoundHttpException("Контакт с ID $id не найден");
+        }
+        return $this->render('view', ['task' => $task]);
     }
 
 }
