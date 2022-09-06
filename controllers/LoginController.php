fd080ea6fdfca62f111516ceb32a5564273f21d7
g123udini <g123udini@gmail.com>
2022-09-06 13:03:53 +0300

    ;

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -18,15 +18,15 @@ class LoginController extends Controller
 
         if (Yii::$app->request->isAjax && $loginForm->load(Yii::$app->request->post())) {
             Yii::$app->response->format = Response::FORMAT_JSON;
-
             ActiveForm::validate($loginForm);
-            }
+
             if ($loginForm->validate()) {
                 echo 'OK';
                 $user = $loginForm->getUser();
                 Yii::$app->user->login($user);
                 $this->goHome();
             }
+        }
 
         return $this->render('landing', ['model' => $loginForm]);
     }
