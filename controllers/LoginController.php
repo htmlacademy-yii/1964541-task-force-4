d0c7cba274d74683edf0547402bbec3bef8ac3bb
g123udini <g123udini@gmail.com>
2022-09-08 10:30:15 +0300

    +

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -16,19 +16,27 @@ class LoginController extends AnonymousController
         $this->layout = 'landing';
         $loginForm = new LoginForm();
 
-        if (Yii::$app->request->isAjax && $loginForm->load(Yii::$app->request->post())) {
-            Yii::$app->response->format = Response::FORMAT_JSON;
-            ActiveForm::validate($loginForm);
-
+        if ($loginForm->load(Yii::$app->request->post())) {
             if ($loginForm->validate()) {
-                echo 'OK';
                 $user = $loginForm->getUser();
                 Yii::$app->user->login($user);
+
                 return $this->goHome();
             }
         }
 
         return $this->render('landing', ['model' => $loginForm]);
+    }
+
+    public function actionValidation()
+    {
+        $this->layout = 'landing';
+        $loginForm = new LoginForm();
+
+        if (Yii::$app->request->isAjax) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            ActiveForm::validate($loginForm);
+        }
     }
 
 }
