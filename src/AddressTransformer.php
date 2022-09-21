9b50898104a47fd596fc2527a493ba58b3087de8
g123udini <g123udini@gmail.com>
2022-09-21 18:15:34 +0300

    ez format works

--- src/AddressTransformer.php
+++ src/AddressTransformer.php
@@ -4,6 +4,10 @@ namespace TaskForce;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\Request;
+use TaskForce\exceptions\BadRequestException;
+use TaskForce\exceptions\WrongAnswerFormatException;
 use yii\db\Exception;
 use yii\helpers\ArrayHelper;
 
@@ -13,6 +17,11 @@ class AddressTransformer
     public $lat;
     public $long;
     const RESPONSE_CODE_OK = 200;
+    const GEOCODE_BASE_URL = 'https://geocode-maps.yandex.ru/';
+    const GEOCODE_API_KEY = 'e666f398-c983-4bde-8f14-e3fec900592a';
+    const GEOCODE_COORDINATES_KEY = 'response.GeoObjectCollection.featureMember.0.GeoObject.Point.pos';
+    const GEOCODE_LONGITUDE = 0;
+    const GEOCODE_LATITUDE = 1;
 
     public function __construct($address)
     {
@@ -21,19 +30,24 @@ class AddressTransformer
 
     public function getLocation()
     {
-        $client = new Client(['base_uri' => 'https://geocode-maps.yandex.ru/']);
-        $response = $client->request('GET', '1.x',
-            ['query' => ['apikey' => 'e666f398-c983-4bde-8f14-e3fec900592a', 'geocode' => $this->address, 'format' => 'json']]);
+        $client = new Client(['base_uri' => self::GEOCODE_BASE_URL]);
+        $request = new Request('GET', '1.x');
+        $response = $client->send($request,
+            ['query' => ['apikey' => self::GEOCODE_API_KEY, 'geocode' => $this->address, 'format' => 'json']]);
 
         if ($response->getStatusCode() !== self::RESPONSE_CODE_OK) {
-            //throw new BadResponseException('MLM');
+            throw new BadRequestException('Ошибка запроса');
         }
         $content = $response->getBody()->getContents();
         $responseData = json_decode($content, true);
 
-        $location = explode(' ', ArrayHelper::getValue($responseData, 'response.GeoObjectCollection.featureMember.0.GeoObject.Point.pos'));
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new WrongAnswerFormatException('Ошибка формата ответа');
+        }
 
-        $this->long = $location[0];
-        $this->lat = $location[1];
+        $location = explode(' ', ArrayHelper::getValue($responseData, self::GEOCODE_COORDINATES_KEY));
+
+        $this->long = $location[self::GEOCODE_LONGITUDE];
+        $this->lat = $location[self::GEOCODE_LATITUDE];
     }
 }
