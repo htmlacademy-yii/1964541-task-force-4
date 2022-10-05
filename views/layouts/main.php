1de996a78b7e8fbfb50ddf7d1355202c9f86c99a
g123udini <g123udini@gmail.com>
2022-10-05 16:37:11 +0300

    main menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -12,6 +12,7 @@ use yii\bootstrap5\Breadcrumbs;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
+use yii\widgets\Menu;
 
 MainAsset::register($this);
 
@@ -30,7 +31,8 @@ $this->beginPage() ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Taskforce</title>
-    <script src="https://api-maps.yandex.ru/2.1/?apikey=<?= Yii::$app->geocoder->getApiKey() ?>&lang=ru_RU" type="text/javascript"></script>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=<?= Yii::$app->geocoder->getApiKey() ?>&lang=ru_RU"
+            type="text/javascript"></script>
     <?php
     $this->head() ?>
 </head>
@@ -38,55 +40,56 @@ $this->beginPage() ?>
 <?php
 $this->beginBody() ?>
 <?php if (!Yii::$app->user->isGuest): ?>
-<div class="<?= $hidden = 'hidden' ?>">
-    <header class="page-header">
-        <nav class="main-nav">
-            <a href='#' class="header-logo">
-                <img class="logo-image" src="<?= Yii::$app->urlManager->baseUrl ?>/img/logotype.png" width=227 height=60
-                     alt="taskforce">
-            </a>
-            <div class="nav-wrapper">
-                <ul class="nav-list">
-                    <li class="list-item list-item--active">
-                        <a href="<?= Yii::$app->urlManager->createUrl('task')?>" class="link link--nav">Новое</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="<?= Yii::$app->urlManager->createUrl(['task/my', 'type' => Task::STATUS_NEW])?>" class="link link--nav">Мои задания</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="<?= Yii::$app->urlManager->createUrl('task/add')?>" class="link link--nav">Создать задание</a>
-                    </li>
-                    <li class="list-item">
-                        <a href="<?= Yii::$app->urlManager->createUrl('user/options') ?>" class="link link--nav">Настройки</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <div class="user-block">
-            <a href="<?= Yii::$app->urlManager->createUrl(['user/view', 'id' => Yii::$app->user->identity->id]) ?>">
-                <img class="user-photo" src="/<?= Yii::$app->user->identity->avatar ?? Yii::$app->urlManager->baseUrl . 'img/man-glasses.png' ?>" width="55"
-                     height="55" alt="Аватар">
-            </a>
-            <div class="user-menu">
-                <p class="user-name"><?= Yii::$app->user->identity->login ?></p>
-                <div class="popup-head">
-                    <ul class="popup-menu">
-                        <li class="menu-item">
-                            <a href="#" class="link">Настройки</a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="#" class="link">Связаться с нами</a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="<?= Yii::$app->urlManager->createUrl('user/logout') ?>" class="link">Выход из
-                                системы</a>
-                        </li>
-                    </ul>
+    <div class="<?= $hidden = 'hidden' ?>">
+        <header class="page-header">
+            <nav class="main-nav">
+                <a href='#' class="header-logo">
+                    <img class="logo-image" src="<?= Yii::$app->urlManager->baseUrl ?>/img/logotype.png" width=227
+                         height=60
+                         alt="taskforce">
+                </a>
+                <div class="nav-wrapper">
+                    <?= Menu::widget(['items' => [
+                        ['label' => 'Новое', 'url' => ['task/']],
+                        ['label' => 'Мои задания', 'url' => ['task/my', 'type' => Task::STATUS_NEW]],
+                        ['label' => 'Создать задание', 'url' => ['task/add']],
+                        ['label' => 'Настройки', 'url' => ['user/options']]],
+                        'options' => [
+                                'class' => 'nav-list'
+                        ],
+                        'linkTemplate' => '<a href="{url}" class="link link--nav">{label}</a>',
+                        'activeCssClass' => 'list-item--active',
+                        'itemOptions' => [ 'class' => 'list-item']])
+                    ?>
+                </div>
+            </nav>
+            <div class="user-block">
+                <a href="<?= Yii::$app->urlManager->createUrl(['user/view', 'id' => Yii::$app->user->identity->id]) ?>">
+                    <img class="user-photo"
+                         src="/<?= Yii::$app->user->identity->avatar ?? Yii::$app->urlManager->baseUrl . 'img/man-glasses.png' ?>"
+                         width="55"
+                         height="55" alt="Аватар">
+                </a>
+                <div class="user-menu">
+                    <p class="user-name"><?= Yii::$app->user->identity->login ?></p>
+                    <div class="popup-head">
+                        <ul class="popup-menu">
+                            <li class="menu-item">
+                                <a href="#" class="link">Настройки</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="#" class="link">Связаться с нами</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="<?= Yii::$app->urlManager->createUrl('user/logout') ?>" class="link">Выход из
+                                    системы</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-</div>
+        </header>
+    </div>
 <?php endif; ?>
 
 <main>
